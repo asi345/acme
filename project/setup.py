12aba88acc03efe8c4ae2f964faf207ba8dd1659
tcinbis <tcinbis@gmail.com>
2020-09-26 14:30:58 +0200

    Adding requirements to setup.py

--- project/setup.py
+++ project/setup.py
@@ -8,4 +8,12 @@ setup(
     license="MIT",
     packages=["src"],
     zip_safe=False,
+    install_requires=[
+        "Flask==1.1.2",
+        "cryptography==3.1",
+        "dnslib==0.9.14",
+        "requests==2.24.0",
+        "pycryptodome==3.9.8",
+        "dacite==1.5.1",
+    ],
 )
