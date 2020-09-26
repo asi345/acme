ccda2457a49eebaf8c811b24c718d8df0f4758a4
tcinbis <tcinbis@gmail.com>
2020-09-26 12:08:28 +0200

    Installing project as package for CI runner

---
+++ project/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="acme-project",
+    version="0.1",
+    description="ACME Client lib",
+    author="Tom Cinbis",
+    license="MIT",
+    packages=["src"],
+    zip_safe=False,
+)
