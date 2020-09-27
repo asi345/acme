5cf0a9a39dd5f9b1067edc8f4021d9a24b37791e
tcinbis <tcinbis@gmail.com>
2020-09-27 15:28:38 +0200

    Debugging testing env

--- project/src/main.py
+++ project/src/main.py
@@ -65,7 +65,7 @@ def main():
     dnsserver.start()
 
     shutdown_thread = threading.Thread(target=start_shutdown_server)
-    demo_thread = threading.Thread(target=start_demo_server, args=(cert_path, key_path, args.domain[0]))
+    demo_thread = threading.Thread(target=start_demo_server, args=(cert_path, key_path))
 
     shutdown_thread.start()
     demo_thread.start()
